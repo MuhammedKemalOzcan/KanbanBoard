171a4122b806b7e4c5220501d3ce2ace9737168e
unknown <muhammedkemal.57@gmail.com>
2025-04-28 14:04:58 +0300

    Revert "düzenleme"
    This reverts commit a2808181dc9333b017115cff39b93e4ae85da4e4.

--- frontend/src/Components/DroppableBoard.jsx
+++ frontend/src/Components/DroppableBoard.jsx
@@ -9,7 +9,7 @@ function DroppableBoard({ board, children }) {
   });
 
   return (
-    <div ref={setNodeRef} className="list">
+    <div ref={setNodeRef} className="list w-[22%] h-auto flex flex-col box-border gap-4">
       {children}
     </div>
   );
